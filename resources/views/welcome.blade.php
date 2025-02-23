1c87228c2cd1b536135e7e00ea1d4bed40562ec3
A42235_Nguyễn Vũ Hương Mai <144614741+HuongMai2172003@users.noreply.github.com>
2025-02-23 17:09:13 +0700

    nhanh3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,7 @@
                                     >
                                         Dashboard545446636363763gdghghhsjsdjd
                                         yytggygyggy
+                                        dhhdhdhd
                                     </a>
                                 @else
                                     <a
@@ -52,6 +53,7 @@
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
                                             hmaiiihdfjjjjjjjjjjgjgghhhhhhhhhhhhhhhh
+                                            hhdjdsghsh
                                         </a>
                                     @endif
                                 @endauth
